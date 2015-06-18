8fc839f62b8f8e33afdef7db1fce6ae11e95e5b4
maortoubian <maortoubian@gmail.com>
2015-06-19 02:12:53 +0300

    try

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 var express = require('express');
+var mongoose = require('./mongoose_connect');
 var app = express();
 
-var mongoose = require('./mongoose_connect');
 
-app.get('/', function(req,res) {
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
 
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Header", "Origin, X-Requested-With,Content-Type, Accept");
-	app.set('json spaces',4);
-	res.set("Content-Type", "application/json");
-	res.status(200);
+//app.get('/', function(req,res) {
 
-	res.json(mongoose.getWords());
+	// res.header("Access-Control-Allow-Origin", "*");
+	// res.header("Access-Control-Allow-Header", "Origin, X-Requested-With,Content-Type, Accept");
+	// app.set('json spaces',4);
+	// res.set("Content-Type", "application/json");
+	//res.status(200);
+	//res.json(mongoose.getWords());
 
-});
+
+//});
 
 
 app.listen(process.env.PORT || 3000);
