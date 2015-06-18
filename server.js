522f729c4770ffcf21b3bcd6aa01adad8c84f662
maortoubian <maortoubian@gmail.com>
2015-06-19 02:26:44 +0300

    please

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var mongoose = require('./mongoose_connect');
 var app = express();
-
+var translator = require('bingtranslator');
 
 app.get('/', function (req, res) {
   res.send('Hello World')
