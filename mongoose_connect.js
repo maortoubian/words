01b22b0a4a45990f50b95483fa32c4e9d368530f
maortoubian <maortoubian@gmail.com>
2015-06-19 02:31:24 +0300

    please

--- mongoose_connect.js
+++ mongoose_connect.js
@@ -3,6 +3,7 @@ mongoose.connect('mongodb://user1:pass1@ds035167.mongolab.com:35167/words');
 //var conn = mongoose.connection;
 
 //var translator = require('bingtranslator');
+var translator = require('bing-translate');
 var sequenty = require ('sequenty');
 
 
